f2d109ae5c043ddc5de580cd453d7de61433e4f1
Rian Finnegan <rian@amphoradata.com>
2019-10-16 10:41:24 +1100

    add more mappings

--- python-WeatherZone/mapping.py
+++ python-WeatherZone/mapping.py
@@ -2,6 +2,8 @@
 # maps WeatherZone location code to an Amphora Id
 def wz_locations():
     return {
-        13388: "85c4dd78-4e70-43fe-950b-7aed543c1a2d"
+        13388: "85c4dd78-4e70-43fe-950b-7aed543c1a2d", # Berri, SA
+        4205: "45b39403-c0c2-42c6-a51a-0c1b8703b7ce", # Albury NSW
+        9396: "7af4c75b-3249-48a0-91a6-42b4a53e7aea" # Spring Hill, Brisbane, QLD
         }
     
