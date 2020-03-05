8045c6e12069bfc03d2436ff336efb19747cc40e
maxmills1 <51426373+maxmills1@users.noreply.github.com>
2020-03-06 01:55:59 +1100

    update signals.py to include rate

--- python-WeatherZone/src/signals.py
+++ python-WeatherZone/src/signals.py
@@ -1,12 +1,13 @@
-from amphora_client import SignalDto
+from amphora_client import Signal
 
 def signals():
     return [
-        SignalDto(_property='description', value_type='String'),
-        SignalDto(_property='temperature', value_type='Numeric'),
-        SignalDto(_property='rainProb', value_type='Numeric'),
-        SignalDto(_property='windSpeed', value_type='Numeric'),
-        SignalDto(_property='windDirection', value_type='Numeric'),
-        SignalDto(_property='cloudCover', value_type='Numeric'),
-        SignalDto(_property='pressure', value_type='Numeric'),
+        Signal(_property='description', value_type='String'),
+        Signal(_property='temperature', value_type='Numeric', attributes={'units': 'degrees C'}),
+        Signal(_property='rainProb', value_type='Numeric', attributes={'units': '%'}),
+        Signal(_property='windSpeed', value_type='Numeric', attributes={'units': 'km/h'}),
+        Signal(_property='windDirection', value_type='Numeric', attributes={'units': 'degrees'}),
+        Signal(_property='cloudCover', value_type='Numeric', attributes={'units': '%'}),
+        Signal(_property='pressure', value_type='Numeric', attributes={'units': 'hPa'}),
+        Signal(_property='rainfallRate', value_type='Numeric', attributes={'units': 'mm/h'})
     ]
