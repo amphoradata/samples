27d9a4423b3b1672555ebbe6c1cc39a95849b47c
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-04-07 14:01:40 +1000

    Update Step_4.py

--- tutorial_pull_realtime_data/Step_4.py
+++ tutorial_pull_realtime_data/Step_4.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Apr  6 17:48:07 2020
-
 @author: Isaac
 """
 
@@ -20,7 +19,6 @@ client = AmphoraDataRepositoryClient(credentials)
 # Get Amphora of interest
 f=open("Amphora_id.txt", "r")
 amphora_id = f.read()
-print(amphora_id)
 
 # Set time points of interest
 start = datetime.utcnow() + timedelta(hours=-48)
@@ -31,7 +29,6 @@ dt_range = DateTimeRange(_from=start, to=end)
 amphora = client.get_amphora(amphora_id)
 signals = amphora.get_signals()
 df = signals.pull(date_time_range=dt_range).to_pandas(t_as_index=False)
-print(df)
 
 # Make plot of data
 df.plot(x ='t', y='timeProduct')
