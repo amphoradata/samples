03dd7cc858c14fc0d235a03da535bf97cf43ab65
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-04-07 10:26:48 +1000

    Update Step_3.py

--- tutorial_pull_realtime_data/Step_3.py
+++ tutorial_pull_realtime_data/Step_3.py
@@ -5,11 +5,7 @@ from amphora_api_client.rest import ApiException
 from amphora_api_client.configuration import Configuration
 
 # Import non-Amphora librarys
-from array import array 
 import os
-import numpy as np
-import time
-from datetime import datetime, timedelta
 
 
 # Login to amphoradata.com
