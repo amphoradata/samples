a486fbe1ac46532d92223305293443d6eaa1bbf7
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-04-07 13:55:45 +1000

    Update Step_3.py

--- tutorial_pull_realtime_data/Step_3.py
+++ tutorial_pull_realtime_data/Step_3.py
@@ -1,14 +1,9 @@
 # Import Amphora librarys
 from amphora.client import AmphoraDataRepositoryClient, Credentials
-import amphora_api_client as a10a
-from amphora_api_client.rest import ApiException
-from amphora_api_client.configuration import Configuration
 
 # Import non-Amphora librarys
 import os
 
-
 # Login to amphoradata.com
 credentials = Credentials(username=os.getenv('username'), password=os.getenv('password')) 
 client = AmphoraDataRepositoryClient(credentials)
-amphora_api = a10a.AmphoraeApi(client.apiClient)
