2a9e7660fefc5e9070a6368e64c8b6e9007be0cf
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-04-08 14:43:36 +1000

    Update Pull_a_signal.py

--- generic_templates/Pull_a_signal.py
+++ generic_templates/Pull_a_signal.py
@@ -1,1 +1,23 @@
+# Import Amphora modules
+from amphora.client import AmphoraDataRepositoryClient, Credentials
 
+# Import non-Amphora modules
+import os
+
+# Login to amphoradata.com
+credentials = Credentials(username=os.getenv('username'), password=os.getenv('password')) 
+client = AmphoraDataRepositoryClient(credentials)
+
+# Get Amphora object
+amphora = client.get_amphora(Amphora_id) 
+
+# Pull signal
+signals = amphora.get_signals()
+
+# Convert signal to Pandas dataframe
+df = signals.pull().to_pandas()
+
+# Alternate: convert signal from specific  time to Pandas dataframe
+from amphora_api_client import DateTimeRange
+time_range = DateTimeRange(_from=start_time, to=end_time)
+df = signals.pull(date_time_range=time_range).to_pandas()
