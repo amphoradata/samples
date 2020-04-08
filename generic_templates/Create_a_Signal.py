8cd782283edc19685e97a85a39dc069a22441bb5
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-04-08 14:33:27 +1000

    Update Create_a_Signal.py

--- generic_templates/Create_a_Signal.py
+++ generic_templates/Create_a_Signal.py
@@ -1,1 +1,15 @@
+# Import Amphora modules
+from amphora.client import AmphoraDataRepositoryClient, Credentials
 
+# Import non-Amphora modules
+import os
+
+# Login to amphoradata.com
+credentials = Credentials(username=os.getenv('username'), password=os.getenv('password')) 
+client = AmphoraDataRepositoryClient(credentials)
+
+# Get Amphora object
+amphora = client.get_amphora(Amphora_id) 
+
+# Create signal
+amphora.create_signal("signal_name", attributes={"units":"XYZ"})
