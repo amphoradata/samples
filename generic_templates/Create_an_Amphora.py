8e2ada46135b2d66625b7bafbbe2e2c6733c396f
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-04-08 15:06:21 +1000

    Expansion of code

--- generic_templates/Create_an_Amphora.py
+++ generic_templates/Create_an_Amphora.py
@@ -9,12 +9,12 @@ credentials = Credentials(username=os.getenv('username'), password=os.getenv('pa
 client = AmphoraDataRepositoryClient(credentials)
 
 # Set metadata for Amphora
-name = #Name the amphora as string
-description = #Describe the amphora as string
-price = #Monthly price as float
-labels = ["Label_1","Label_2","Label_3"]
-lat = #Enter latitude as float
-lon = #Enter longitude as float
+name = "Amphora_Name" #Name the amphora as string
+description = "Amphora_Description" #Describe the amphora as string
+price = 0 #Monthly price as float
+labels = ["Label_1","Label_2","Label_3"]  #List of 2-5 one word strings
+lat = 0.0 #Enter latitude as float
+lon = 0.0 #Enter longitude as float
 
 # Create Amphora
-amphora = client.create_amphora(name=name, description=desc, labels=labels, price=price, lat=lat, lon = lon)
+amphora = client.create_amphora(name=name, description=description, labels=labels, price=price, lat=lat, lon = lon)
