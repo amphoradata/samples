195be507f882e941b5700427a3597c6ac22e1a4b
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-04-08 14:37:12 +1000

    Update Push_a_file.py

--- generic_templates/Push_a_file.py
+++ generic_templates/Push_a_file.py
@@ -1,1 +1,17 @@
+# Import Amphora modules
+from amphora.client import AmphoraDataRepositoryClient, Credentials
+
+# Import non-Amphora modules
+import os
+
+# Login to amphoradata.com
+credentials = Credentials(username=os.getenv('username'), password=os.getenv('password')) 
+client = AmphoraDataRepositoryClient(credentials)
+
+# Get Amphora object
+amphora = client.get_amphora(Amphora_id) 
+
+# Push file to amphora
+file_path="path/to/your/file"
+amphora.push_file(amphora.Amphora_id, file_path)
 
