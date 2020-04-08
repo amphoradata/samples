2b1a0ad791fd0161b5b5a3e932a6d62086384b3f
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-04-08 14:38:20 +1000

    Update Pull_a_file.py

--- generic_templates/Pull_a_file.py
+++ generic_templates/Pull_a_file.py
@@ -1,1 +1,15 @@
+# Import Amphora modules
+from amphora.client import AmphoraDataRepositoryClient, Credentials
 
+# Import non-Amphora modules
+import os
+
+# Login to amphoradata.com
+credentials = Credentials(username=os.getenv('username'), password=os.getenv('password')) 
+client = AmphoraDataRepositoryClient(credentials)
+
+# Get Amphora object
+amphora = client.get_amphora(Amphora_id) 
+
+# Pull file
+amphora.get_file(file_name).pull(destination_file_path)
