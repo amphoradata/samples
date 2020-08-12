6b38f470d0102b12f8d9cdc35124d0affd3bbcb4
Rian Finnegan <rian@amphoradata.com>
2020-08-12 16:20:47 +1000

    add constants

---
+++ glaze_dashboard/python/jobs/constants.py
@@ -0,0 +1,8 @@
+insights_id = "ae086aeb-0275-4072-a636-0c19c713cf41"
+weather_observation_id = "8abb5605-3f7a-419e-bb8f-b4791b6d6534"
+weather_forecasts_id = "57d6593f-1889-410a-b1fb-631b6f9c9c85"
+river_union_bridge_id = "52f4fec3-18e7-4f5a-948c-3a021d7b9ae8"
+river_doctors_point_id = "910a761c-8a26-4cf6-83d7-b1b3589caf01"
+
+max_temperature_threshold = 10
+min_temperature_threshold = -2
