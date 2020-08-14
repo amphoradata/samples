2d7077f27445e0558765ac2c6b7e7e4638a64d5e
Rian Finnegan <rian@amphoradata.com>
2020-08-14 15:06:47 +1000

    fix: set reasonable thresholds

--- glaze_dashboard/python/jobs/constants.py
+++ glaze_dashboard/python/jobs/constants.py
@@ -2,5 +2,5 @@ insights_id = "ae086aeb-0275-4072-a636-0c19c713cf41"
 weather_observation_id = "8abb5605-3f7a-419e-bb8f-b4791b6d6534"
 weather_forecasts_id = "57d6593f-1889-410a-b1fb-631b6f9c9c85"
 
-max_temperature_threshold = 10
-min_temperature_threshold = 10
+max_temperature_threshold = 38
+min_temperature_threshold = 1
